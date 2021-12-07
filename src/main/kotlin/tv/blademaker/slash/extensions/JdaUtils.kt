e2859acc61ad87471de13bf20f837a91a56ef611
Blad3Mak3r <blademaker.live@gmail.com>
2021-12-07 14:26:59 +0100

    Add Jda utils.

---
+++ src/main/kotlin/tv/blademaker/slash/extensions/JdaUtils.kt
@@ -0,0 +1,23 @@
+package tv.blademaker.slash.extensions
+
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder
+import net.dv8tion.jda.api.sharding.ShardManager
+import tv.blademaker.slash.api.SlashCommandClient
+
+fun JDABuilder.setSlashCommandClient(slashCommandClient: SlashCommandClient) {
+    addEventListeners(slashCommandClient)
+}
+
+fun JDA.setSlashCommandClient(slashCommandClient: SlashCommandClient) {
+    addEventListener(slashCommandClient)
+}
+
+fun DefaultShardManagerBuilder.setSlashCommandClient(slashCommandClient: SlashCommandClient) {
+    addEventListeners(slashCommandClient)
+}
+
+fun ShardManager.setSlashCommandClient(slashCommandClient: SlashCommandClient) {
+    addEventListener(slashCommandClient)
+}
