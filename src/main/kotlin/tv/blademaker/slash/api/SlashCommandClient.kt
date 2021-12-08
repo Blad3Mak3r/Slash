696ceac67dd52a1e5313432da014671f21ada68a
Blad3Mak3r <blademaker.live@gmail.com>
2021-12-08 14:15:17 +0100

    Add documentations

--- src/main/kotlin/tv/blademaker/slash/api/SlashCommandClient.kt
+++ src/main/kotlin/tv/blademaker/slash/api/SlashCommandClient.kt
@@ -11,24 +11,45 @@ import tv.blademaker.slash.internal.SlashUtils.toHuman
 @Suppress("unused")
 interface SlashCommandClient : EventListener {
 
+    /**
+     * The command registry.
+     */
     val registry: List<BaseSlashCommand>
 
     override fun onEvent(event: GenericEvent) {
         if (event is SlashCommandEvent) onSlashCommandEvent(event)
     }
 
+    /**
+     * Handled when discord send an [SlashCommandEvent]
+     *
+     * @param event The [SlashCommandEvent] sent by Discord.
+     */
     fun onSlashCommandEvent(event: SlashCommandEvent)
 
     fun getCommand(name: String) = registry.firstOrNull { it.commandName.equals(name, true) }
 
+    /**
+     * Enable prometheus metrics exporters
+     */
     fun withMetrics() {
         Metrics.register()
     }
 
+    /**
+     * Register the event listener to receive [SlashCommandEvent] from your [ShardManager].
+     *
+     * @param shardManager The [ShardManager]
+     */
     fun withShardManager(shardManager: ShardManager) {
         shardManager.addEventListener(this)
     }
 
+    /**
+     * Register the event listener to receive [SlashCommandEvent] from your [JDA] shard.
+     *
+     * @param jda The [JDA] shard instance.
+     */
     fun withJDA(jda: JDA) {
         jda.addEventListener(this)
     }
