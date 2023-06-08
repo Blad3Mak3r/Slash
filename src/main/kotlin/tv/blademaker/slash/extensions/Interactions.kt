7cf07cc88368852246583f080ce5f4bb6679b80e
Blad3Mak3r <juanluiscarobenito@gmail.com>
2023-06-08 23:18:36 +0200

    Optimize some imports

--- src/main/kotlin/tv/blademaker/slash/extensions/Interactions.kt
+++ src/main/kotlin/tv/blademaker/slash/extensions/Interactions.kt
@@ -2,8 +2,6 @@ package tv.blademaker.slash.extensions
 
 import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent
 import net.dv8tion.jda.api.events.interaction.command.GenericCommandInteractionEvent
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
-import net.dv8tion.jda.api.utils.messages.MessageCreateData
 
 val GenericCommandInteractionEvent.commandPath: String
     get() = buildString {
