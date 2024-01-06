bc938ac1bd1132077ac05cb6f8726f9a325838eb
Blad3Mak3r <juanluiscarobenito@gmail.com>
2024-01-06 11:20:45 +0100

    Include jda and selfUser inside context directly.

--- src/main/kotlin/tv/blademaker/slash/context/InteractionContext.kt
+++ src/main/kotlin/tv/blademaker/slash/context/InteractionContext.kt
@@ -1,5 +1,7 @@
 package tv.blademaker.slash.context
 
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.entities.User
 import net.dv8tion.jda.api.events.GenericEvent
 import net.dv8tion.jda.api.events.interaction.GenericInteractionCreateEvent
 import net.dv8tion.jda.api.interactions.Interaction
@@ -12,6 +14,12 @@ interface InteractionContext<E : GenericInteractionCreateEvent> {
     val event: E
 
     val client: SlashCommandClient
+
+    val jda: JDA
+        get() = event.jda
+
+    val selfUser: User
+        get() = event.jda.selfUser
 
     val interaction: Interaction
         get() = event.interaction
