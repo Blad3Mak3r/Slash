5c2b8cc2f9d2a3f745470a3292be2a8f51862c4d
Blad3Mak3r <juanluiscarobenito@gmail.com>
2026-06-06 23:47:35 +0200

    chore: update dependencies versions on libs.versions.toml

--- slash-ksp-processor/build.gradle.kts
+++ slash-ksp-processor/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.publish)
+    alias(libs.plugins.versions)
 
     `java-library`
     signing
