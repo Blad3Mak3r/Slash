ba813d4fc3544e14b475f8f8f54c688921b225d9
Blad3Mak3r <juanluiscarobenito@gmail.com>
2026-06-29 14:04:15 +0200

    fix(annotations): swap OnSlashCommand params so name comes before group

--- slash-annotations/src/main/kotlin/io/github/blad3mak3r/slash/annotations/OnSlashCommand.kt
+++ slash-annotations/src/main/kotlin/io/github/blad3mak3r/slash/annotations/OnSlashCommand.kt
@@ -3,16 +3,16 @@ package io.github.blad3mak3r.slash.annotations
 /**
  * Marks a suspend function as the handler for a [CommandType.SLASH] command (or subcommand).
  *
- * @param group Optional subcommand-group name.
  * @param name  Optional subcommand name.
+ * @param group Optional subcommand-group name.
  * @param target The [InteractionTarget] this handler accepts.
  * @param supportDetached Whether the command supports detached guild messages.
  */
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.SOURCE)
 annotation class OnSlashCommand(
-    val group: String = "",
     val name: String = "",
+    val group: String = "",
     val target: InteractionTarget = InteractionTarget.GUILD,
     val supportDetached: Boolean = false
 )
